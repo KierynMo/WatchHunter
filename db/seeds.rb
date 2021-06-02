2eb97509403e23a70e4ef8e532d707031b09b55e
Kieryn Moore <kierynmoore@hotmail.com>
2021-06-02 14:42:55 +0100

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ require "open-uri"
 filename_iterator = 1
 
 
-
+User.create(first_name: "Doug", last_name: "Barr", email: "doug@gmail.com", password: '12345')
 20.times do
   @user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '123456')
   watch = Watch.create!(name: Faker::Superhero.name, brand: Faker::Company.name, price: rand(2...8000), user: @user)
