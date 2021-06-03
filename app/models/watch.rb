65962b01ca16b4e357bc7b3aab904bd9afee50c9
Timothy Martin <tpmartin7@gmail.com>
2021-06-03 15:48:39 +0100

    New watch successfully geocodes with postcode

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -2,7 +2,9 @@ class Watch < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
-  
+  geocoded_by :postcode
+  after_validation :geocode, if: :will_save_change_to_postcode?
+
   include PgSearch::Model
   pg_search_scope :search_by_name_and_brand,
     against: [ :name, :brand ],
