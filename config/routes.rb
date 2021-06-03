37a8aa101e4c39a14efac0f3b86b68164ceb310c
Doug Barr <douglas.barr01@gmail.com>
2021-06-03 16:41:26 +0100

    added destroy method for watches

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :watches, only: [:index, :show, :new, :create] do
+  resources :watches, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:new, :create, :destroy]
   end
   resources :users, only: [:new, :create, :show]
