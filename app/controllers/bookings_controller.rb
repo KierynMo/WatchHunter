ac0795521c5b678bac7e05a82e413156132fb16e
Kieryn Moore <kierynmoore@hotmail.com>
2021-06-03 13:03:53 +0100

    bookings now have a total price column that is calculated when they book. watch cards are changed to price/day and booking cards have the total price on them

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,16 +7,20 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.user_id = current_user.id
-    # @booking.user = current_user
+    @watch = Watch.find(params[:watch_id])
     @booking.watch_id = params[:watch_id]
-      if @booking.save
-        redirect_to user_path(current_user.id)   # redirects to the user show page - shows all their bookings etc
-      else
-        render :new
-      end
+    price = (@booking.end_date - @booking.start_date)
+    @booking.total_price = price.to_i * @watch.price
+
+    if @booking.total_price > 0 && @booking.save
+      redirect_to user_path(current_user.id)   # redirects to the user show page - shows all their bookings etc
+    else
+      render :new
+    end
   end
 
   private
+
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_114312) do
+ActiveRecord::Schema.define(version: 2021_06_03_104431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_114312) do
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "total_price"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["watch_id"], name: "index_bookings_on_watch_id"
   end
